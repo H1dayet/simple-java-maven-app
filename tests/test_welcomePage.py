c0ba2779ce6c96b47372012e3fc9a06931a1621a
H1dayet <hidayet1352@gmail.com>
2025-03-29 13:02:08 +0400

    Added a test

---
+++ tests/test_welcomePage.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+class TestWelcomePage:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.set_window_size(1670, 989)
+        yield
+        self.driver.quit()
+
+    def test_welcome_page_navigation(self):
+        self.driver.get("http://localhost:8080/")
+        self.driver.find_element(By.LINK_TEXT, "Go to Dashboard").click()
+        assert "Dashboard" in self.driver.title or "dashboard" in self.driver.current_url.lower()
